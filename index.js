118e6b086923be8b99eb1472d0369ac7192fa7c0
Forhad <farhadhossain01992@gmail.com>
2022-10-25 21:50:34 +0600

    added

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    res.send('Courses API running')
+});
+
+app.get('/all-curseName', (req,res) =>{
+    res.send(coursesInfo);
+})
+
+app.listen(port , () =>{
+    console.log('Courses server running on port', port);
+})
